8a25e1f930f20092317c4242206b027e80bb5fc0
Harsh Joshi <Harsh.Joshi@deltacubes.us>
2025-06-15 14:06:48 +0530

    Added Cerifiy

--- config/database.py
+++ config/database.py
@@ -1,16 +1,25 @@
+import os
 import certifi
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
+# Load environment variables from a .env file (only needed for local dev)
+load_dotenv()
+
+# Get the Mongo URI from environment variable
+MONGO_URI = os.environ.get("MONGO_URI")
+
+# Create Mongo client with TLS and CA file
 client = MongoClient(
-    "mongodb+srv://kellyharrisoninfo:1gNy7ZxN8VoQHDE9@project0.e1kmvyv.mongodb.net/?retryWrites=true&w=majority&appName=Project0",
+    MONGO_URI,
     tls=True,
     tlsCAFile=certifi.where()
 )
 
-# Use one database
+# Select database
 db = client.realestate_db
 
-# Define multiple collections
+# Define collections
 customer_collection = db["customer_collection"]
 properties_collection = db["properties_collection"]
 users_collection = db["users"]
