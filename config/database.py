96bad46a62cf6b49203b20604cb2e2e4d731867e
Harsh Joshi <Harsh.Joshi@deltacubes.us>
2025-06-15 14:51:07 +0530

    Added Cerifiy

--- config/database.py
+++ config/database.py
@@ -1,19 +1,13 @@
 from pymongo import MongoClient
-import os
 import certifi
+import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
 load_dotenv()
 
-# Get the connection string from environment variable
 MONGO_URI = os.getenv("MONGO_URI")
-
-# Connect to MongoDB Atlas with proper TLS certificate
 client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
-
-# Access your database (replace with your actual DB name if needed)
-db = client.get_database()
+db = client.get_default_database()
 
 # Define collections
 customer_collection = db["customer_collection"]
